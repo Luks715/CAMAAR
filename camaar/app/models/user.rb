a21903b41237262b6b67b33165e954d100243ed7
Lucas Teixeira <lucastsciencia@gmail.com>
2024-06-18 21:03:12 -0300

    ultimas atualizações

--- camaar/app/models/user.rb
+++ camaar/app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
     # Inclua os módulos do Devise necessários
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_one :docente
+  has_one :dicente
 
   validates :nome, presence: true
   validates :email, presence: true
   validates :password, presence: true
-  validates :type, presence: true
   validates :usuario, presence: true
   validates :formacao, presence: true
 end
--- camaar/db/schema.rb
+++ camaar/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_18_170539) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_222322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,7 +105,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_170539) do
     t.string "nome", null: false
     t.string "email", null: false
     t.string "password", null: false
-    t.string "type", default: "Dicente", null: false
     t.string "usuario", null: false
     t.string "formacao", null: false
     t.datetime "created_at", null: false
