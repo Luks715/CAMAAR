73878a3ba84deae75859b5bafe18ebc552d7d166
Lucas Teixeira de Souza <161272902+Luks715@users.noreply.github.com>
2024-07-05 23:08:26 -0300

    Update turma.rb

--- camaar/app/models/turma.rb
+++ camaar/app/models/turma.rb
@@ -1,6 +1,6 @@
 class Turma < ApplicationRecord
   belongs_to :disciplina
-  belongs_to :docente
+  belongs_to :docente, optional: true
 
   has_and_belongs_to_many :dicentes, join_table: 'turmas_dicentes'
   has_and_belongs_to_many :formularios, dependent: :destroy, join_table: 'formularios_turmas'
